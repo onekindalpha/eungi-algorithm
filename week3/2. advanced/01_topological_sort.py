aaa94e5d1cbe4fa2fb7199b279c05b15eb4e21e3
velocitygoal <velocitygoal@gmail.com>
2026-09-07 15:09:34 +0900

    위상정렬.py

--- week3/2. advanced/01_topological_sort.py
+++ week3/2. advanced/01_topological_sort.py
@@ -40,21 +40,48 @@ def topological_sort(vertices, edges):
         위상 정렬 순서
     """
     # TODO: 그래프와 진입 차수 초기화
-    pass
-    
-    # TODO: 그래프 구성 및 진입 차수 계산
-    pass
-    
-    # TODO: 진입 차수가 0인 정점들을 큐에 추가
-    pass
-    
+    n = vertices
+    graph = {}
     result = []
-    
+    # 진입차수를 저장할 곳임. 
+    indegree = [0] * n 
+    # 그래프를 초기화함. 
+    for vertice in range(vertices):
+        graph[vertice] = []
+    # 간선에 대한 그래프를 만들고, v에 대해서는 진입차수를 하나 추가함. 
+    # 그냥 v라고만 하는 이유는 indegree가 0부터 시작함. 
+    for u, v in edges:
+        graph[u].append(v)
+        indegree[v] +=1
+    # 문서 대기열을 만들고
+    queue=deque()
+    # 방문한 노드는 더이상 방문하지 않기로 함. 
+    visited = [False] * n
+    # 각 노드들에 대해 
+    for vertice in range(vertices):
+        # 진입차수가 0인 정점들을 큐에 추가하고. 
+        if indegree[vertice] ==0:
+            queue.append(vertice)
+            visited[vertice] = True
+            # TODO: 진입 차수가 0인 정점들을 큐에 추가
     # TODO: 큐가 빌 때까지 반복
-    ## 큐에서 정점 꺼내기
-    ## 인접한 정점들의 진입 차수 감소
-    pass
-    
+    while queue:
+        ## 큐에서 정점 꺼내기
+        c_node = queue.popleft()
+        # pop을 한 순서가 위상정렬임. 
+        result.append(c_node)
+        ## 아 근데 인접한 정점이네 - 인ㅈ버한 노드들에 대해서만. 
+        for next_node in graph[c_node]:
+        ## 인접한 정점들의 진입 차수 감소
+        ## 인접한 정점 노드의 숫자가 나오겠지
+            indegree[next_node] -= 1
+        # 마만약 진입차수가 0이 되면 푸시를 함
+        # 남은 것에 대해 돌아야 하나......
+        # 큐에서 하나 팝했으면 팝한 만큼 range를 주라고 한 것 같은데 . 
+        for i in range(n):
+            if indegree[i] == 0 and not visited[i]:
+                queue.append(i)
+                visited[i] = True
     return result
 
 # 테스트 케이스
